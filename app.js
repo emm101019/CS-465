c22841b1d62730ecff4fbeb3fbc17ad4136c6b4b
Eva M <emm101019@gmail.com>
2026-04-12 17:17:11 -0700

    Module 5 API routes and controllers

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const hbs = require('hbs');
 require('./app_server/models/db');
+
 const app = express();
 
 app.set('view engine', 'hbs');
@@ -10,7 +11,10 @@ app.set('views', path.join(__dirname, 'app_server', 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 const travelRouter = require('./app_server/routes/index');
+const apiRouter = require('./app_api/routes/index');
+
 app.use('/', travelRouter);
+app.use('/api', apiRouter);
 
 app.listen(3000, () => {
   console.log('Server running on port 3000');
